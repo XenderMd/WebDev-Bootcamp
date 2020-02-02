7273610913d8d04a85a628b608020115a1e61dcf
XenderMd <denis.stavilamd@gmail.com>
2020-02-02 17:31:46 +0100

    Understanding and working with routes

--- Web Development/my-express-server/server.js
+++ Web Development/my-express-server/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(request, response){
-    response.send("<h1>Hello World!</h1>");
+app.get("/", function(req, res){
+    res.send("<h1>Hello World!</h1>");
 });
+
+app.get("/contact", function(req, res){
+    res.send("Contact me at: email@domain.com");
+});
+
+app.get("/about", function(req, res){
+    res.send("This is a starter express server application, with demo data");
+});
+
+app.get("/hobbies", function(req, res){
+    res.send("My hobbies are programming + gaming :)")
+})
 
 app.listen(3000, function (){
     console.log("Server started on port 3000");
