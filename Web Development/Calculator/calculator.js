80ab8f01e99f94e8f940b6bad84781b925efc8d2
XenderMd <denis.stavilamd@gmail.com>
2020-02-02 19:52:21 +0100

    BMI calculator (and simple calculator) implementation

--- Web Development/Calculator/calculator.js
+++ Web Development/Calculator/calculator.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 const port = 3000;
 
 app.get("/", function(req, res){
@@ -7,6 +10,27 @@ app.get("/", function(req, res){
     console.log(file);
     res.sendFile(file);
 });
+
+app.get("/bmiCalculator", function(req, res){
+    let file = __dirname+"/bmiCalculator.html";
+    res.sendFile(file);
+})
+
+app.post("/bmiCalculator", function (req, res){
+    let weight = Number(req.body.weight);
+    let height = Number(req.body.height);
+    let bmi=weight/Math.pow(height,2);
+    console.log(height);
+    res.send("Your BMI calculation is: "+Math.round(bmi));
+})
+
+app.post("/", function (req, res){
+
+    let num1=Number(req.body.num1);
+    let num2=Number(req.body.num2);
+    let result= num1+num2;
+    res.send("The result of the calculation is: " + result);
+})
 
 app.listen(port, ()=>{
     console.log(`Started listening on port ${port}`);
