465bc3cc7f4ea38535815f0a7ab625539bff1720
StavilaD <denis.stavila@prodsp.hu>
2020-03-10 14:22:35 +0100

    Add Environment Vars

--- Web Development/Secrets/app.js
+++ Web Development/Secrets/app.js
@@ -1,24 +1,24 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require ("mongoose-encryption");
+
  
 const app = express();
- 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true, useUnifiedTopology: true});
 const Schema = mongoose.Schema;
-const secret = "Thisisourlittlesecret.";
 const userSchema = new Schema({
     email:String,
     password:String
 });
-userSchema.plugin(encrypt, {secret:secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
 const User = new mongoose.model("User", userSchema);
 
 
