2c5ce09e2a2831f1106ab3818ff0d1d12f29e42f
StavilaD <denis.stavila@prodsp.hu>
2020-03-11 14:43:00 +0100

    Finished project -> Letting Users Submit Secrets

--- Web Development/Secrets/app.js
+++ Web Development/Secrets/app.js
@@ -32,12 +32,15 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     email:String,
     password:String,
-    googleId:String
+    googleId:String,
+    secret:String
 });
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
 
 passport.serializeUser(function(user, done) {
     done(null, user.id);
@@ -87,18 +90,43 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    }else{
-        res.redirect("/login");
-    }
+   User.find({"secret":{$ne:null}}, function(err, foundUsers){
+       if(err){console.log(err);}
+       else{
+           if(foundUsers){
+               res.render("secrets", {usersWithSecrets:foundUsers});
+           }
+       }
+   });
 });
+
+app.get("/submit", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }
+    else {
+        res.render("login");
+    }
+})
 
 app.get("/logout", function(req, res){
     req.logOut();
     res.redirect('/');
 });
 
+app.post("/submit", function(req, res){
+    const submittedSecret=req.body.secret;
+    console.log(req.user.id);
+    User.findById(req.user.id, function(err, foundUser){
+        if(err) {console.log(err);}
+        else{
+            if(foundUser){
+                foundUser.secret=submittedSecret;
+                foundUser.save(function(err){res.redirect("/secrets")});
+            }
+        }
+    });
+});
 
 app.post("/register", function(req, res){
     User.register({username: req.body.username}, req.body.password, function(err, user){
