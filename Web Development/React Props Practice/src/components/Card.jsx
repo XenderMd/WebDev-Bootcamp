313b96d33cc9ac89d571c2ccedc1520ed0bb1316
XenderMd <denis.stavilamd@gmail.com>
2020-03-24 12:23:25 +0100

    React Props

---
+++ Web Development/React Props Practice/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+{console.log("contacts")}
+
+export default function Card (props){
+
+    return (
+        <div className="card">
+          <div className="top">
+            <h2 className="name">{props.name}</h2>
+            <img
+              className="circle-img"
+              src={props.imgURL}
+              alt="avatar_img"
+            />
+          </div>
+          <div className="bottom">
+            <p className="info">{props.phone}</p>
+            <p className="info">{props.email}</p>
+          </div>
+        </div>
+    );
+};
+
