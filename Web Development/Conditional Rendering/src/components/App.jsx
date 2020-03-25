9a7a1cfbc7172294b7bc7712b5b9492a1f9a7322
XenderMd <denis.stavilamd@gmail.com>
2020-03-25 17:08:53 +0100

    Conditional Rendering

---
+++ Web Development/Conditional Rendering/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./Login";
+
+let isLoggedIn = false;
+const currentTime = new Date().getHours();
+console.log(currentTime);
+
+function App() {
+  return (
+    <div className="container">
+      { isLoggedIn ? <h1>Hello</h1> : <Login/>}
+      
+    </div>
+  );
+}
+
+export default App;
