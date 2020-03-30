79fa2d264e18b46725076f4388024fd4c922d1e5
XenderMd <denis.stavilamd@gmail.com>
2020-03-30 20:48:50 +0200

    Finished Kepper App Project - Part 3

--- Web Development/Keeper App Part3/src/components/App.jsx
+++ Web Development/Keeper App Part3/src/components/App.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [notes, setNotes] = useState([{title:"Example Note", content:"Random Content"}]);
+
+  function addNote(note){
+    setNotes((prevState)=>{return [...prevState, note]});
+  }
+
+  function deleteNote(ID){
+    setNotes((prevState)=>{return prevState.filter((item, index)=>{return index!==ID;}) })
+  }
+
+  function displayNotes(notes){
+    return (
+      notes.map((note, index)=>{return(<Note key={index} id={index} title={note.title} content={note.content} ifDelete={deleteNote}/>)})
+    )
+  }
+
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea ifAdd={addNote}/>
+      {displayNotes(notes)}
       <Footer />
     </div>
   );
--- Web Development/Keeper App Part3/src/components/CreateArea.jsx
+++ Web Development/Keeper App Part3/src/components/CreateArea.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+  const [note, setNote] = useState({title:"", content:""})
+
+  function onTitleChange(event){
+    const newTitle = event.target.value;
+    setNote((prevState)=>{return(
+      {
+        ...prevState,
+        ["title"]:newTitle
+      }
+    )})
+  }
+
+  function onContentChange(event){
+    const newContent = event.target.value;
+    setNote((prevState)=>{return(
+      {
+        ...prevState,
+        ["content"]:newContent
+      }
+    )})
+  }
+
+  function addHandler(event){
+    props.ifAdd(note);
+    event.preventDefault();
+  }
+  
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" placeholder="Title" value ={note.title} onChange={onTitleChange}/>
+        <textarea name="content" onChange={onContentChange} value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={addHandler}>Add</button>
       </form>
     </div>
   );
