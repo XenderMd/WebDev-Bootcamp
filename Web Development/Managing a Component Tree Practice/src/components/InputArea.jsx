097d3cabd9b676c19faa37502e037aafa9803195
XenderMd <denis.stavilamd@gmail.com>
2020-03-30 18:22:30 +0200

    Managing a component tree practice

---
+++ Web Development/Managing a Component Tree Practice/src/components/InputArea.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+function InputArea(props) {
+
+  const [inputText, setInputText] = useState("");
+  
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function handleClick(){
+    props.ifClicked(inputText)
+    setInputText("");
+  }
+
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button onClick={handleClick}>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
